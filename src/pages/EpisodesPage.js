443682700cbb0a514dac883dc29695920b044daf
Ramón Pazos <devrampe@gmail.com>
2020-11-02 10:42:46 +0100

    design episodesPage

--- src/pages/EpisodesPage.js
+++ src/pages/EpisodesPage.js
@@ -1,13 +1,15 @@
+import { Container } from '@material-ui/core';
 import React, { Fragment, useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { getAllEpisodesBySeason } from '../redux/actions/episodes.actions';
-
+import { List, ListItem, Button } from '@material-ui/core';
 
 //INCLUIR NUEVO ESTADO: QUOTE
 const mapStateToProps = (state) =>{
     return {
-        EPISODES_SEASON: state.EPISODES_SEASON
+        EPISODES_SEASON: state.EPISODES_SEASON,
+        GLOBAL: state.GLOBAL
     };
 };
 
@@ -31,19 +33,28 @@ const _EpisodesPage = ({getAllEpisodesBySeason,...props})=> {
     },[])
     return (
         <Fragment>
-            <h1>EPISODES PAGE</h1>
-            {
-                (props.EPISODES_SEASON.DATA_RESPONSE.length>0) &&
-                props.EPISODES_SEASON.DATA_RESPONSE.map((episode,index)=>{
-                    return (
-                        <li key={ index }>
-                            { episode.title }
-                            <br></br>
-                            <button onClick={()=>handleRedirectToEpisodeDetail(episode)} >IR A DETALLES</button>
-                        </li>
-                    )
-                })
-            }
+            <header>
+                 <div className="jumbotron">
+                    <h1 className="display-4">Lista de episodios de la temporada : {props.GLOBAL.SEASON_SELECTED}</h1>
+                </div>
+           </header>
+           <Container>
+               {
+                   (props.EPISODES_SEASON.LOADING) ? <h2>Cargando ....</h2> :
+                   (props.EPISODES_SEASON.DATA_RESPONSE.length>0) &&
+                   <List>
+                       {
+                           props.EPISODES_SEASON.DATA_RESPONSE.map((episode)=>{
+                               return (
+                                   <ListItem key={episode.episode_id}>
+                                     <Button fullWidth={true}  variant="outlined" onClick={ ()=> handleRedirectToEpisodeDetail(episode) }>{episode.title}</Button>
+                                   </ListItem>
+                               )
+                           })
+                       }
+                   </List>
+               }
+           </Container>
         </Fragment>
     );
 }
